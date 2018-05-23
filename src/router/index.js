cfda3f8f16e59605a80564eaead3bd5b5860844d
ewall <1054064180@qq.com>
2018-05-23 18:17:56 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Test from '@/view/test'
-import Title1 from '@/view/Title1'
-import Title2 from '@/view/Title2'
 import Goods from '@/view/goods'
 
 Vue.use(Router)
