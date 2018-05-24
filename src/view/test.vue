a8aad57bc1bbb8abc811323a195e08b11040dc9b
ewall <1054064180@qq.com>
2018-05-24 11:00:56 +0800

    动态路由

---
+++ src/view/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test">
+  	This is id: {{$route.params.testId}}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello vue'
+    }
+
+  }
+}
+
+</script>
+<style scoped></style>
