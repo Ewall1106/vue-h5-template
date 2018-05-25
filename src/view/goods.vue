637b5769e73a86c8938e7c5bb7a80b3d2b2da105
ewall <1054064180@qq.com>
2018-05-25 21:14:38 +0800

    编程式路由

---
+++ src/view/goods.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="goods">
+		This is goods:
+		<span>{{$route.query.goodsId}}</span>
+	</div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped></style>
