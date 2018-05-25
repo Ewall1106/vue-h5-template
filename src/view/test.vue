f7ac1c8e01ab1194d54c26d8402e51ea6d1fc9e8
ewall <1054064180@qq.com>
2018-05-25 10:03:48 +0800

    嵌套路由

--- src/view/test.vue
+++ src/view/test.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="test">
+  	<!-- 动态路由 -->
   	This is id: {{$route.params.testId}}
+
+  	<!-- 嵌套路由 -->
+  	<router-link to="/test/title1">标题1</router-link>
+  	<router-link to="/test/title2">标题2</router-link>
+  	<router-view></router-view>
   </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      msg: 'hello vue'
-    }
-  }
-}
 
+}
 </script>
 <style scoped></style>
