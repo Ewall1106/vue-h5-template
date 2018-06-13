b9ca9ed84374eed7e38e81e001db0929f6030412
ewall <1054064180@qq.com>
2018-06-13 15:17:58 +0800

    'add'

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -27,6 +27,7 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'styles': resolve('src/assets/styles')
     }
   },
   module: {
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import Router from 'vue-router'
 import Test from '@/view/test'
 import Title1 from '@/view/title1'
 import Title2 from '@/view/title2'
-import Goods from '@/view/goods'
 
 Vue.use(Router)
 
@@ -12,7 +11,7 @@ export default new Router({
     path: '/',
     name: '/',
     components: {
-      default: Goods,
+      default: Test,
       title1: Title1,
       title2: Title2
     }
