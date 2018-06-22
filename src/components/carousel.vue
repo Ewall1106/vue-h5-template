ad1c513a8008db58db06962e243f1eca5696944a
ewall <1054064180@qq.com>
2018-06-22 17:04:58 +0800

    'add'

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -6,10 +6,10 @@
         <img class="myswiper_img" src="http://a3.vimage1.com/upload/flow/2018/06/21/160/15295761713116.jpg" alt="">
       </swiper-slide>
       <swiper-slide>
-        <img class="myswiper_img" src="https://a3.vimage1.com/upload/flow/2018/06/06/29/15282588215380.jpg" alt="">
+        <img class="myswiper_img" src="https://a2.vimage1.com/upload/flow/2018/06/22/40/15296362191561.jpg" alt="">
       </swiper-slide>
       <swiper-slide>
-        <img class="myswiper_img" src="https://a2.vimage1.com/upload/flow/2018/06/22/40/15296362191561.jpg" alt="">
+        <img class="myswiper_img" src="https://a3.vimage1.com/upload/flow/2018/06/06/29/15282588215380.jpg" alt="">
       </swiper-slide>
       <swiper-slide>
         <img class="myswiper_img" src="https://a4.vimage1.com/upload/flow/2018/06/21/49/15295697893682.jpg" alt="">
@@ -49,6 +49,9 @@ export default {
 
 <style lang="less" scoped>
 .carousel {
+  /deep/ .swiper-pagination-bullet-active {
+    background: #fff;
+  }
   .myswiper_img {
     width: 100%;
     height: 4rem;
