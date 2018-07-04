7ad1ee821884b5c40a36449584a45ebcb3b837e1
ewall <1054064180@qq.com>
2018-07-04 11:38:28 +0800

    'add'

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,41 +1,17 @@
 <template>
   <div class="home">
     <my-header></my-header>
-    <carousel></carousel>
+    <carousel :swiperData='swiperData'></carousel>
     <!-- 分类 -->
     <div class="classification">
       <div class="class_top">
-        <div class="item">
-          <img src="https://a4.vimage1.com/upload/flow/2018/05/29/106/15275616596915.jpg" alt="">
-        </div>
-        <div class="item">
-          <img src="https://a2.vimage1.com/upload/flow/2018/05/22/138/15269786875124.jpg" alt="">
-        </div>
-        <div class="item">
-          <img src="https://a4.vimage1.com/upload/flow/2018/05/29/100/15275815231657.jpg" alt="">
-        </div>
-        <div class="item">
-          <img src="https://a4.vimage1.com/upload/flow/2018/05/22/190/15269788745661.jpg" alt="">
-        </div>
-        <div class="item">
-          <img src="https://a4.vimage1.com/upload/flow/2018/05/22/110/15269789816830.jpg" alt="">
+        <div class="item" v-for="(item,index) in classifiData.up" :key="index">
+          <img :src="item" alt="">
         </div>
       </div>
       <div class="class_bottom">
-        <div class="item">
-          <img src="https://a3.vimage1.com/upload/flow/2018/05/29/40/15275616172415.jpg" alt="">
-        </div>
-        <div class="item">
-          <img src="https://a2.vimage1.com/upload/flow/2018/05/29/139/15275815452999.jpg" alt="">
-        </div>
-        <div class="item">
-          <img src="https://a3.vimage1.com/upload/flow/2018/05/22/163/15269793276446.jpg" alt="">
-        </div>
-        <div class="item">
-          <img src="https://a4.vimage1.com/upload/flow/2018/05/22/172/15269794028123.jpg" alt="">
-        </div>
-        <div class="item">
-          <img src="https://a4.vimage1.com/upload/flow/2018/05/22/21/15269794589634.jpg" alt="">
+        <div class="item" v-for="(item,index) in classifiData.down" :key="index">
+          <img :src="item" alt="">
         </div>
       </div>
     </div>
@@ -43,28 +19,11 @@
     <div class="recommend">
       <div class="title">今日推荐</div>
       <div class="content">
-        <div class="item">
-          <img src="https://b.vimage1.com/upload/mst/2018/06/27/14/9b74b67ad8d6049035e8178f6c17ab70_604x290_80.jpg" alt="">
+        <div class="item" v-for="(item,index) in recommendData" :key="index">
+          <img :src="item.imgurl" alt="">
           <div class="item_text">
-            <span>运动潮人录取线
-            </span>
-            <span>剩 2 天</span>
-          </div>
-        </div>
-        <div class="item">
-          <img src="https://b.vimage1.com/upload/mst/2018/06/26/189/f43d77bad367edf2cd9721601f90df1a_604x290_80.jpg" alt="">
-          <div class="item_text">
-            <span>国潮攻心计
-            </span>
-            <span>剩 1 天</span>
-          </div>
-        </div>
-        <div class="item">
-          <img src="https://a.vimage1.com/upcb/2018/06/05/132/ias_152816727785388_604x290_80.jpg" alt="">
-          <div class="item_text">
-            <span>欧美品牌女装与美泽美
-            </span>
-            <span>剩 2 天</span>
+            <span>{{item.title}}</span>
+            <span>剩 {{item.endtime}} 天</span>
           </div>
         </div>
       </div>
@@ -74,7 +33,7 @@
 <script>
 import MyHeader from "@/components/myHeader";
 import Carousel from "@/components/carousel";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "home",
@@ -83,7 +42,14 @@ export default {
     Carousel
   },
   data() {
-    return {};
+    return {
+      // 轮播图
+      swiperData: [],
+      // 分类
+      classifiData: {},
+      // 推荐
+      recommendData: []
+    };
   },
   mounted() {
     this.getIndexData();
@@ -92,7 +58,10 @@ export default {
     getIndexData() {
       axios.get("/api/index.json").then(res => {
         console.log(res.data);
-      })
+        this.swiperData = res.data.swiper;
+        this.classifiData = res.data.classifiData;
+        this.recommendData = res.data.recommendData;
+      });
     }
   }
 };
@@ -130,7 +99,7 @@ export default {
     .content {
       .item {
         background: #fff;
-        margin-bottom: .133333rem;
+        margin-bottom: 0.133333rem;
         img {
           width: 100%;
           height: 4.8rem;
