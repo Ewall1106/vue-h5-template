e18133c8b67027b0142c9d19ea83abe01f664569
ewall <1054064180@qq.com>
2020-03-30 13:35:46 +0800

    add

--- src/api/detail.js
+++ src/api/detail.js
@@ -1,12 +1,5 @@
 import request from '@/utils/request'
 
-// export function getDetail() {
-//   return request({
-//     url: '/home/banner',
-//     method: 'get'
-//   })
-// }
-
 export function getDetail(data) {
   return request({
     url: '/goods/detail',
