27d804020c74f09040bfe328bc3c891c08ecaec5
ewall <1054064180@qq.com>
2020-04-03 15:55:23 +0800

    add

--- mock/index.js
+++ mock/index.js
@@ -9,11 +9,13 @@ import cart from './cart'
 
 const mocks = [...user, ...home, ...category, ...product, ...detail, ...cart]
 
-// const delayApi = [
-//   '/prod-api/user/login',
-//   '/prod-api/home/list',
-//   '/prod-api/product/list'
-// ]
+const delayApi = [
+  '/prod-api/user/login',
+  '/prod-api/home/list',
+  '/prod-api/product/list',
+  '/prod-api/goods/detail',
+  '/prod-api/cart/list'
+]
 
 // https://github.com/nuysoft/Mock/wiki
 export function mockXHR() {
@@ -21,10 +23,10 @@ export function mockXHR() {
     Mock.mock(item.url, item.type, item.response)
 
     // 为了效果演示，定义一些接口延时
-    // if (delayApi.includes(item.url)) {
-    //   Mock.setup({
-    //     timeout: 1500
-    //   })
-    // }
+    if (delayApi.includes(item.url)) {
+      Mock.setup({
+        timeout: 1000
+      })
+    }
   }
 }
