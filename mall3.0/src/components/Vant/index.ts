f99502d1fd5de34e7d1d10498c22bb1549feeb0f
ewall <1054064180@qq.com>
2021-01-13 16:01:04 +0800

    reafactor：support typescript

--- mall3.0/src/components/Vant/index.ts
+++ mall3.0/src/components/Vant/index.ts
@@ -1,7 +1,8 @@
 import { App } from 'vue'
-import { Button, List, Cell, Tabbar, TabbarItem } from 'vant'
+import { Button, List, Tabbar, TabbarItem } from 'vant'
 
-const plugins = [Button, List, Cell, Tabbar, TabbarItem]
+// https://vant-contrib.gitee.io/vant/v3/#/zh-CN
+const plugins = [Button, List, Tabbar, TabbarItem]
 
 export default {
   install: function(vm: App) {
@@ -10,105 +11,3 @@ export default {
     })
   }
 }
-
-// https://youzan.github.io/vant/#/zh-CN/home
-// import {
-//   Button,
-//   Toast,
-//   Dialog,
-//   Field,
-//   Form,
-//   Notify,
-//   Cell,
-//   CellGroup,
-//   NavBar,
-//   Search,
-//   Image,
-//   Lazyload,
-//   Swipe,
-//   SwipeItem,
-//   Loading,
-//   Tag,
-//   Tabbar,
-//   TabbarItem,
-//   List,
-//   PullRefresh,
-//   TreeSelect,
-//   Icon,
-//   Progress,
-//   ImagePreview,
-//   Rate,
-//   GoodsAction,
-//   GoodsActionIcon,
-//   GoodsActionButton,
-//   Sku,
-//   AddressList,
-//   AddressEdit,
-//   SwipeCell,
-//   Card,
-//   SubmitBar,
-//   Checkbox,
-//   CheckboxGroup,
-//   Grid,
-//   GridItem,
-//   DropdownMenu,
-//   DropdownItem,
-//   ContactCard,
-//   ContactList,
-//   ContactEdit,
-//   ShareSheet,
-//   Tab,
-//   Tabs,
-//   Sticky,
-//   Empty
-// } from 'vant'
-
-// app
-//   .use(Toast)
-//   .use(Dialog)
-//   .use(Notify)
-//   .use(Field)
-//   .use(Form)
-//   .use(Button)
-//   .use(Cell)
-//   .use(CellGroup)
-//   .use(NavBar)
-//   .use(Search)
-//   .use(Image)
-//   .use(Lazyload)
-//   .use(Swipe)
-//   .use(SwipeItem)
-//   .use(Loading)
-//   .use(Tag)
-//   .use(Tabbar)
-//   .use(TabbarItem)
-//   .use(List)
-//   .use(PullRefresh)
-//   .use(TreeSelect)
-//   .use(Icon)
-//   .use(Progress)
-//   .use(ImagePreview)
-//   .use(Rate)
-//   .use(GoodsAction)
-//   .use(GoodsActionIcon)
-//   .use(GoodsActionButton)
-//   .use(Sku)
-//   .use(AddressList)
-//   .use(AddressEdit)
-//   .use(SwipeCell)
-//   .use(Card)
-//   .use(SubmitBar)
-//   .use(Checkbox)
-//   .use(CheckboxGroup)
-//   .use(Grid)
-//   .use(GridItem)
-//   .use(DropdownMenu)
-//   .use(DropdownItem)
-//   .use(ContactCard)
-//   .use(ContactList)
-//   .use(ContactEdit)
-//   .use(ShareSheet)
-//   .use(Tab)
-//   .use(Tabs)
-//   .use(Sticky)
-//   .use(Empty)
--- mall3.0/src/utils/config.ts
+++ mall3.0/src/utils/config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   mock: {
     baseUrl: '/dev-api',
     mockUrl: 'https://mock.xwhx.top'
