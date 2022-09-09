3ab72c2e73c0e4d8f490be4cde763d2138501861
danielxiong <ewall1106@gmail.com>
2022-09-09 15:12:47 +0800

    feat: description

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,41 +1,14 @@
 <template>
-  <div class="login-container">
-    <div class="header">
-      <!-- <img class="header__logo" src="@/assets/logo.png" alt="logo" /> -->
-      <!-- <p class="header__title">Vue-H5-Template</p> -->
-    </div>
+  <div class="home">
+    <Info />
   </div>
 </template>
 
 <script>
+import Info from "@/components/Info.vue";
+
 export default {
-  name: "Login",
-  data() {
-    return {};
-  },
+  name: "Home",
+  components: { Info },
 };
 </script>
-
-<style lang="scss" scoped>
-.login-container {
-  .header {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    .header__logo {
-      display: block;
-      width: 150px;
-      height: 150px;
-      padding-top: 80px;
-    }
-    .header__title {
-      font-size: 36px;
-      color: #000;
-      font-weight: normal;
-      padding-top: 30px;
-    }
-  }
-}
-</style>
