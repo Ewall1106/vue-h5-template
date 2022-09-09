10958a2f54206c4e23608a02d9f2f59b96755efa
xiongwei <>
2022-09-09 17:14:40 +0800

    chores

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from "vue";
-
-import "normalize.css/normalize.css";
-import "./styles/index.scss";
-
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+import "normalize.css/normalize.css";
+import "./styles/index.scss";
 
 if (process.env.NODE_ENV === "development") {
   const { mockXHR } = require("../mock");
