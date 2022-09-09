b5de7e083927d360d6abe1cd092be4f3af0e726a
danielxiong <ewall1106@gmail.com>
2022-09-09 21:42:23 +0800

    feat: mock online demo

--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ import store from "./store";
 import "normalize.css/normalize.css";
 import "./styles/index.scss";
 
-if (process.env.NODE_ENV === "development") {
-  const { mockXHR } = require("../mock");
-  mockXHR();
-}
+// if (process.env.NODE_ENV === "development") {
+const { mockXHR } = require("../mock");
+mockXHR();
+// }
 
 Vue.config.productionTip = false;
 
